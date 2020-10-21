de4a4fa030975c853e1be9acaae05fa4756f2710
siyangc <csy602994381@gmail.com>
2020-10-21 23:40:42 +1100

    add first image

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-
+import {simpleImage} from '../imagePatterns/simpleImage'
+import {imageOne} from '../imagePatterns/ImageOne'
 export default class canvas extends Component {
     constructor(props){
         super(props)
@@ -10,37 +11,28 @@ export default class canvas extends Component {
         marginLeft:'100px',
         marginTop:'100px'
     }
+
     componentDidMount(){
         this.produceImage()
     }
+
     produceImage = () => {
         const ctx = this.canvas.current.getContext('2d')
 
         const image = ctx.createImageData(256,128)
 
-        let d = 0
-        
-        for(let i=8;i<257;i+=8){
-            for(let j=8;j<257;j+=8){
-                for(let k=8;k<257;k+=8){
-                    image.data[d+0] = i
-                    image.data[d+1] = j
-                    image.data[d+2] = k
-                    image.data[d+3] = 255
-                    d+=4
-                }
-            }
-        }
-        ctx.putImageData(image,0,0)
+        //ctx.putImageData(simpleImage(image),0,0)
+        ctx.putImageData(imageOne(image),0,0)
     }
+
     render() {
         return (
             <div>
                 <canvas
                  style={this.canvasStyle}
                  ref={this.canvas}
-                 width={300}
-                 height={300}
+                 width={256}
+                 height={128}
                 />
             </div>
         )
---
+++ src/imagePatterns/ImageOne.js
@@ -0,0 +1,18 @@
+export const imageOne = (image) =>{
+    let d = 0
+    for(let hx=1;hx<33;hx++){
+        for(let hz=1;hz<5;hz++){
+            for(let wy=1;wy<33;wy++){
+                for(let wz=1;wz<9;wz++){
+                    image.data[d+0] = hx * 8
+                    image.data[d+1] = wy * 8
+                    image.data[d+2] = hz * wz * 8 
+                    image.data[d+3] = 255
+                    d += 4
+                }
+            }
+        }
+    }
+
+    return image
+}
